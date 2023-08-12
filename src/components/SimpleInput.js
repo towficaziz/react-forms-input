d789f4e410e1c9e5804ed2c4903e7d1aae9d7992
towficaziz <tapolly01@gmail.com>
2023-08-12 23:28:48 +0200

    Managing the overall form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,9 +3,16 @@ import { useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== ''; 
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+    if(enteredNameIsValid){
+      formIsValid = true;
+    } 
 
   const nameInputChaneHandler = (event) => {
     setEnteredName(event.target.value);
@@ -55,7 +62,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
